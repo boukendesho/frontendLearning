7ffdf190349fac60f4ab9a069248c844a8584420
boukendesho <100831251+boukendesho@users.noreply.github.com>
2026-02-21 23:36:25 +0900

    ec-site: fix header images issue in react

--- react/react-ecommerce/src/App.jsx
+++ react/react-ecommerce/src/App.jsx
@@ -9,29 +9,29 @@ function App() {
     <>
       <Header />
       <main>
-        <div class="product-container">
-          <div class="item-container">
+        <div className="product-container">
+          <div className="item-container">
             <img
-              class="item-image"
+              className="item-image"
               src="../../images/products/athletic-cotton-socks-6-pairs.jpg"
               alt="socks"
             />
-            <div class="item-text">
+            <div className="item-text">
               Black and Gray Athletic Cotton Socks - 6 Pairs
             </div>
-            <div class="item-rating">
+            <div className="item-rating">
               <img
-                class="item-rating-image"
+                className="item-rating-image"
                 src="../../images/ratings/rating-45.png"
                 alt="rating star"
               />
-              <a class="item-rating-link" href="#">
+              <a className="item-rating-link" href="#">
                 87
               </a>
             </div>
-            <div class="item-price">$10.90</div>
+            <div className="item-price">$10.90</div>
             <select
-              class="item-quantity-selector"
+              className="item-quantity-selector"
               name="item-quantity"
               id="quantity-select"
             >
@@ -41,28 +41,28 @@ function App() {
               <option value="4">4</option>
               <option value="5">5</option>
             </select>
-            <button class="item-cart-button">Add to Cart</button>
+            <button className="item-cart-button">Add to Cart</button>
           </div>
-          <div class="item-container">
+          <div className="item-container">
             <img
-              class="item-image"
+              className="item-image"
               src="../../images/products/intermediate-composite-basketball.jpg"
               alt="socks"
             />
-            <div class="item-text">Intermediate Size Basketball</div>
-            <div class="item-rating">
+            <div className="item-text">Intermediate Size Basketball</div>
+            <div className="item-rating">
               <img
-                class="item-rating-image"
+                className="item-rating-image"
                 src="../../images/ratings/rating-40.png"
                 alt="rating star"
               />
-              <a class="item-rating-link" href="#">
+              <a className="item-rating-link" href="#">
                 127
               </a>
             </div>
-            <div class="item-price">$20.95</div>
+            <div className="item-price">$20.95</div>
             <select
-              class="item-quantity-selector"
+              className="item-quantity-selector"
               name="item-quantity"
               id="quantity-select"
             >
@@ -72,28 +72,28 @@ function App() {
               <option value="4">4</option>
               <option value="5">5</option>
             </select>
-            <button class="item-cart-button">Add to Cart</button>
+            <button className="item-cart-button">Add to Cart</button>
           </div>
-          <div class="item-container">
+          <div className="item-container">
             <img
-              class="item-image"
+              className="item-image"
               src="../../images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg"
               alt="socks"
             />
-            <div class="item-text">Adults Plain Cotton T-Shirt - 2 Pack</div>
-            <div class="item-rating">
+            <div className="item-text">Adults Plain Cotton T-Shirt - 2 Pack</div>
+            <div className="item-rating">
               <img
-                class="item-rating-image"
+                className="item-rating-image"
                 src="../../images/ratings/rating-45.png"
                 alt="rating star"
               />
-              <a class="item-rating-link" href="#">
+              <a className="item-rating-link" href="#">
                 56
               </a>
             </div>
-            <div class="item-price">$7.99</div>
+            <div className="item-price">$7.99</div>
             <select
-              class="item-quantity-selector"
+              className="item-quantity-selector"
               name="item-quantity"
               id="quantity-select"
             >
@@ -103,7 +103,7 @@ function App() {
               <option value="4">4</option>
               <option value="5">5</option>
             </select>
-            <button class="item-cart-button">Add to Cart</button>
+            <button className="item-cart-button">Add to Cart</button>
           </div>
         </div>
       </main>
--- react/react-ecommerce/src/Header.jsx
+++ react/react-ecommerce/src/Header.jsx
@@ -1,39 +1,38 @@
 import "./Header.css";
-export function Header() {
+import Logo from "./assets/amazon-logo-white.png";
+import SearchArrow from "./assets/font-awesome-right-arrow.svg";
+import CartIcon from "./assets/font-awesome-cart-white.svg";
+
+function Header() {
   return (
     <>
       <header>
-        <section class="left-section">
+        <section className="left-section">
           <a href="/">
-            <img src="../../images/amazon-logo-white.png" alt="site logo" />
+            <img src={Logo} alt="site logo" />
           </a>
         </section>
-        <section class="middle-section">
+        <section className="middle-section">
           <input type="search" name="search" id="search" placeholder="Search" />
           <div>
             <button type="button">
-              <img
-                src="../../images/font-awesome-right-arrow.svg"
-                alt="search icon"
-              />
+              <img src={SearchArrow} alt="search icon" />
             </button>
           </div>
         </section>
-        <section class="right-section">
-          <a class="orders-link" href="orders">
+        <section className="right-section">
+          <a className="orders-link" href="orders">
             Orders
           </a>
-          <a class="cart-link" href="checkout">
-            <img
-              class="cart-icon"
-              src="../../images/font-awesome-cart-white.svg"
-              alt="cart icon"
-            />
-            <div class="cart-quantity">3</div>
-            <div class="cart-text">Cart</div>
+          <a className="cart-link" href="checkout">
+            <img className="cart-icon" src={CartIcon} alt="cart icon" />
+            <div className="cart-quantity">3</div>
+            <div className="cart-text">Cart</div>
           </a>
         </section>
       </header>
     </>
   );
 }
+
+export default Header;
